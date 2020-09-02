4eb0f8b1bb443bdd48701836691e4830bd8897f8
Stephen Moye <stephenmoye@gmail.com>
2020-09-01 23:09:02 -0700

    restaurant bill completed

--- Chapter 2/RestaurantBill.java
+++ Chapter 2/RestaurantBill.java
@@ -4,6 +4,27 @@
 // The tip should be 20 percent of the total after adding the tax. Display the meal charge, tax
 // amount, tip amount, and total bill on the screen.
 
-public class RestaurantBill {
+import javax.swing.JOptionPane;
 
+public class RestaurantBill {
+  public static void main(String[] args) {
+    String bill;
+    double price;
+    double tax;
+    double tip;
+    double total;
+
+    bill = JOptionPane.showInputDialog("How much was your bill?");
+
+    price = Double.parseDouble(bill);
+    tax = price * 0.0675;
+    tip = (price + tax) * 0.2;
+    total = price + tip + tax;
+
+    System.out.println("Bill amount: $" + bill);
+    System.out.println("Taxes: $" + tax);
+    System.out.println("Amount tipped: $" + tip);
+    System.out.println("Grand Total: $" + total);
+
+  }
 }
