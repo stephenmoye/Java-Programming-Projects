7822ab255aa2545271020024ad0aaa38af460c81
Stephen Moye <stephenmoye@gmail.com>
2020-09-14 21:58:03 -0700

    converted TestAverage to use Scanner

--- Chapter 2/TestAverage.java
+++ Chapter 2/TestAverage.java
@@ -2,24 +2,28 @@
 // Write a program that asks the user to enter three test scores. 
 // The program should display each test score, as well as the average of the scores.
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class TestAverage {
   public static void main(String[] args) {
-    String test1 = JOptionPane.showInputDialog("First test score");
-    String test2 = JOptionPane.showInputDialog("Second test score");
-    String test3 = JOptionPane.showInputDialog("Third test score");
+    int test1;
+    int test2;
+    int test3;
 
-    int score1 = Integer.parseInt(test1);
-    int score2 = Integer.parseInt(test2);
-    int score3 = Integer.parseInt(test3);
+    Scanner input = new Scanner(System.in);
 
-    int average = (score1 + score2 + score3) / 3;
+    System.out.print("First test score: ");
+    test1 = input.nextInt();
+    System.out.print("Second test score: ");
+    test2 = input.nextInt();
+    System.out.print("Third test score: ");
+    test3 = input.nextInt();
 
-    System.out.println("First test score: " + score1);
-    System.out.println("Second test score: " + score2);
-    System.out.println("Third test score: " + score3);
+    int average = (test1 + test2 + test3) / 3;
+
     System.out.println("Overall test average: " + average);
+
+    input.close();
 
   }
 }
