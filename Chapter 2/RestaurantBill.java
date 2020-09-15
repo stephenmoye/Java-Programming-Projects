7d3d1c75b8e2a1086f929c147dbf16f846131f87
Stephen Moye <stephenmoye@gmail.com>
2020-09-14 21:39:03 -0700

    converted RestaurantBill to use Scanner instead of JOptionPane

--- Chapter 2/RestaurantBill.java
+++ Chapter 2/RestaurantBill.java
@@ -4,27 +4,30 @@
 // The tip should be 20 percent of the total after adding the tax. Display the meal charge, tax
 // amount, tip amount, and total bill on the screen.
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class RestaurantBill {
   public static void main(String[] args) {
-    String bill;
     double price;
     double tax;
     double tip;
     double total;
 
-    bill = JOptionPane.showInputDialog("How much was your bill?");
+    Scanner input = new Scanner(System.in);
 
-    price = Double.parseDouble(bill);
+    System.out.print("How much was your bill?");
+    price = input.nextDouble();
+
     tax = price * 0.0675;
     tip = (price + tax) * 0.2;
     total = price + tip + tax;
 
-    System.out.println("Bill amount: $" + bill);
+    System.out.println("Bill amount: $" + price);
     System.out.println("Taxes: $" + tax);
     System.out.println("Amount tipped: $" + tip);
     System.out.println("Grand Total: $" + total);
+
+    input.close();
 
   }
 }
