56075216f6c204529573fde632b1fd5fa4016c2f
Stephen Moye <stephenmoye@gmail.com>
2020-10-19 19:27:09 -0700

    directory changes

---
+++ Chapter 1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.print("Hello World");
+  }
+}
+
+// Source file must have same name as PUBLIC CLASS (case sensitive)
+// .java extension is required as well
