27163b0521e1acc3b32479039e835fd5d1b4c7f0
Stephen Moye <stephenmoye@gmail.com>
2020-11-24 19:20:01 -0800

    largestandsmallest.java completed

--- Chapter 4/LargestAndSmallest.java
+++ Chapter 4/LargestAndSmallest.java
@@ -1,7 +1,34 @@
 // Largest and Smallest
-// Write a program with a loop that lets the user enter a series of integers. The user should
-// enter −99 to signal the end of the series. After all the numbers have been entered, the program should display the largest and smallest numbers entered.
+// Write a program with a loop that lets the user enter a series of integers. 
+// The user should enter −99 to signal the end of the series. 
+// After all the numbers have been entered, the program should 
+// display the largest and smallest numbers entered.
+
+import java.util.Scanner;
 
 public class LargestAndSmallest {
-  
+  public static void main(String[] args) {
+    int num, smallest, largest;
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter a number. -99 will end the program.");
+    System.out.print("Please enter a number: ");
+    num = input.nextInt();
+    smallest = num;
+    largest = num;
+
+    while (num != -99) {
+      System.out.print("Please enter a number: ");
+      num = input.nextInt();
+      if (num == -99) {
+        break;
+      } else if (num < smallest) {
+        smallest = num;
+      } else if (num > largest) {
+        largest = num;
+      }
+    }
+    System.out.println("Smallest: " + smallest);
+    System.out.print("Largest: " + largest);
+  }
 }
