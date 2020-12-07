6e1b2c9c1247ca8e9f356bbd8bd61e2f1b06d58c
Stephen Moye <stephenmoye@gmail.com>
2020-12-06 21:13:18 -0800

    solved RectangleArea.java

--- Chapter 5/RectangleArea.java
+++ Chapter 5/RectangleArea.java
@@ -13,32 +13,56 @@
 // // •	 displayData—This method should accept the rectangle’s length, width, and area as
 // // arguments, and display them in an appropriate message on the screen
 
-
-
-
-
-
 // // Insert any necessary import statements here.
+import java.util.Scanner;
 
+public class RectangleArea {
 
-// public class AreaRectangle
-// {
-//    public static void main(String[] args)
-//    {
-//       double length,    // The rectangle's length
-//              width,     // The rectangle's width
-//              area;      // The rectangle's area
-   
-//       // Get the rectangle's length from the user.
-//       length = getLength();
-   
-//       // Get the rectangle's width from the user.
-//       width = getWidth();
+  public static void main(String[] args) {
+    double length, // The rectangle's length
+        width, // The rectangle's width
+        area; // The rectangle's area
 
-//       // Get the rectangle's area.
-//       area = getArea(length, width);
+    // Get the rectangle's length from the user.
+    length = getLength();
 
-//       // Display the rectangle data.
-//       displayData(length, width, area);
-//    }
-// }
+    // Get the rectangle's width from the user.
+    width = getWidth();
+
+    // Get the rectangle's area.
+    area = getArea(length, width);
+
+    // Display the rectangle data.
+    displayData(length, width, area);
+  }
+
+  public static double getLength() {
+    double length;
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Length(cm): ");
+    length = input.nextInt();
+
+    return length;
+  }
+
+  public static double getWidth() {
+    double width;
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Width(cm): ");
+    width = input.nextInt();
+
+    return width;
+  }
+
+  public static double getArea(double length, double width) {
+    double area = length * width;
+    return area;
+  }
+
+  public static void displayData(double length, double width, double area) {
+    System.out.println(length + " L |" + width + " W");
+    System.out.println("Area: " + area + " cm²");
+  }
+}
